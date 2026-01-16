b54e65b6aa7a3e514e4aefea01c1090f422eddc8
ThienKe <lamthienke135@gmail.com>
2026-01-17 06:38:10 +0700

    chuyển cấu hình vào biến môi trường

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,14 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyC4lErq3P7YaKrqZWdlW_God9Atg_GtWqk",
-  authDomain: "phim-cu-dem.firebaseapp.com",
-  databaseURL: "https://phim-cu-dem-default-rtdb.firebaseio.com",
-  projectId: "phim-cu-dem",
-  storageBucket: "phim-cu-dem.firebasestorage.app",
-  messagingSenderId: "839795184262",
-  appId: "1:839795184262:web:7459607b931c747d9b3707",
-  measurementId: "G-GLPJDCQB56"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Khởi tạo Firebase
